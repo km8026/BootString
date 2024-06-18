a583f5b097fe9216a8e48b083a4f92b550af5941
MinJunKim <167392999+km8026@users.noreply.github.com>
2024-06-18 16:51:21 +0900

    gheg

--- src/main/java/com/example/basic/controller/ThymeleafController.java
+++ src/main/java/com/example/basic/controller/ThymeleafController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.basic.entity.Emp;
 import com.example.basic.entity.Hospital;
@@ -17,20 +18,57 @@ import com.example.basic.repository.HospitalRepository;
 
 @Controller
 public class ThymeleafController {
+
+  @GetMapping("insert1")
+  public String insert1() {
+    return "insert1";
+  }
+
+  @GetMapping("layout1")
+  public String layout1() {
+    return "layout1";
+  }
+
+  @GetMapping("layout2")
+  public String layout2() {
+    return "layout2";
+  }
+
+  @GetMapping("pagination")
+  public String pagination(
+      Model model, @RequestParam(defaultValue = "1") int page) {
+    int startPage = (page - 1) / 10 * 10 + 1;
+    int endPage = startPage + 9;
+    model.addAttribute("startPage", startPage);
+    model.addAttribute("endPage", endPage);
+    model.addAttribute("page", page);
+    return "pagination";
+  }
+
+  @GetMapping("mode")
+  public String mode(
+      Model model, @RequestParam Map<String, Object> map) {
+    model.addAttribute("now", map.get("now") == null ? "am" : map.get("now"));
+    model.addAttribute("name", map.get("name"));
+    model.addAttribute("auth", map.get("auth"));
+    model.addAttribute("category", map.get("category"));
+    return "mode";
+  }
+
   @Autowired
   HospitalRepository hospitalRepository;
 
   @GetMapping("/hospitallist")
-  public String hospi(Model model){
-    List<Hospital> hospitals= hospitalRepository.findAll();
+  public String hospi(Model model) {
+    List<Hospital> hospitals = hospitalRepository.findAll();
     model.addAttribute("hospitals", hospitals);
-                       // hospitals 와 html안에 변수명 동일
+    // hospitals 와 html안에 변수명 동일
     return "hospital";
   }
 
   @Autowired
   EmpRepository empRepository;
- 
+
   @GetMapping("/emplist")
   public String empList(Model model) {
     List<Emp> empList = empRepository.findAll();
@@ -40,7 +78,7 @@ public class ThymeleafController {
 
   @GetMapping("userList")
   public String userList(Model model) {
-    
+
     List<Map<String, Object>> userList = new ArrayList<>();
     Map<String, Object> user = null;
     user = new HashMap<>();
